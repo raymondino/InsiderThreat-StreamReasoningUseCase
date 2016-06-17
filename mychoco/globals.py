e91bc7d7f4a01691001136d6cda93c8f021e74e4
Liyu Pan <lenapan0521@gmail.com>
2016-06-17 15:11:21 -0400

    Update globals.py

--- mychoco/globals.py
+++ mychoco/globals.py
@@ -1,4 +1,5 @@
-# global.py
+# globals.py
+# containing global variables
 cloudStorageWebsites = ['http://www.4shared.com/','http://1and1.com/','https://archive.org/','http://bluehost.com/','http://bp.blogspot.com/','http://yousendit.com/','http://yfrog.com/','http://webs.com/','http://twitpic.com/','http://soundcloud.com/','http://secureserver.net/','http://custhelp.com/','http://megaupload.com/','http://megaclick.com/','http://hostgator.com/','http://flippa.com/','http://dropbox.com/']
 hacktivistWebsites = ['http://wikileaks.com']
 jobHuntingWebsites = ['http://careerbuilder.com/','http://craigslist.org/','http://simplyhired.com/','http://monster.com/','http://jobhuntersbible.com/','http://job-hunt.org/','http://indeed.com/','http://linkedin.com/']
