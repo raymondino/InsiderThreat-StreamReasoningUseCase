fa5e76c2cbefac24925c98fdb8b8befb93856ebe
mychoco <lenapan0521@gmail.com>
2016-06-30 15:51:05 -0400

    background.py added

--- mychoco/decoyfile2rdf.py
+++ mychoco/decoyfile2rdf.py
@@ -4,8 +4,8 @@ from globals import *
 def decoyToRDF(record):
     record = record.strip().split(',')
     filename = record[0].replace('\\','_')
-    print '<%s%s> <%s> <%sDecoyFile>' %(ex,filename,a,ex)
-    print '<%s%s> <%sisIn> <%s%s>' %(ex,filename,ex,ex,record[1])
+    print '<%s%s> <%s> <%sDecoyFile>.' %(ex,filename,a,ex)
+    print '<%s%s> <%sisIn> <%s%s>.' %(ex,filename,ex,ex,record[1])
 
 if __name__ == '__main__':
     f = open('decoy_file.csv')
