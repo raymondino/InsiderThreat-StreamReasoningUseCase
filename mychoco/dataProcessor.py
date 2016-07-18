e2a57a2b4ce55e509ed2b29438c02378fecb7c6d
Rui Yan <raymondino.yan@gmail.com>
2016-07-18 16:11:10 -0400

    update timestamp annotation

--- mychoco/dataProcessor.py
+++ mychoco/dataProcessor.py
@@ -46,31 +46,31 @@ def tsToStr(timestamp):
 def logon(record,outfile):
     id = record[1][1:len(record[1])-1]
     timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-    print >>outfile, '<%sevent%s> <%shasAction> <%slogon_%s>.' %(ex,record[3],ex,ex,id)
-    print >>outfile, '<%slogon_%s> <%s> <%s%sAction>.|%s' %(ex,id,a,ex,record[5],tsToStr(timestamp))
-    print >>outfile, '<%slogon_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
+    print >>outfile, '<%s%s-event> <%shasAction> <%slogon_%s>. |%s' %(ex,record[3],ex,ex,id,tsToStr(timestamp))
+    print >>outfile, '<%slogon_%s> <%s> <%s%sAction>.' %(ex,id,a,ex,record[5])
+    #print >>outfile, '<%slogon_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
     if timestamp.time() < dailyStart or timestamp.time() > dailyEnd:
-        print >>outfile, '<%slogon_%s> <%s> <%sAfterHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%slogon_%s> <%s> <%sAfterHourAction>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%slogon_%s> <%s> <%sInHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%slogon_%s> <%shasActor> <%s%s>.|%s' %(ex,id,ex,ex,record[3],tsToStr(timestamp))
-    print >>outfile, '<%slogon_%s> <%sisPerformedOnPC> <%s%s>.|%s' %(ex,id,ex,ex,record[4],tsToStr(timestamp))
+        print >>outfile, '<%slogon_%s> <%s> <%sInHourAction>.' %(ex,id,a,ex)
+    print >>outfile, '<%slogon_%s> <%shasActor> <%s%s>.' %(ex,id,ex,ex,record[3])
+    print >>outfile, '<%slogon_%s> <%sisPerformedOnPC> <%s%s>.' %(ex,id,ex,ex,record[4])
 
 def device(record,outfile):
     id = record[1][1:len(record[1])-1]
     timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-    print >>outfile, '<%sevent%s> <%shasAction> <%sdevice_%s>.' %(ex,record[3],ex,ex,id)
-    print >>outfile, '<%sdevice_%s> <%s> <%s%sAction>.|%s' %(ex,id,a,ex,'Disk'+record[6]+'ion',tsToStr(timestamp))
-    print >>outfile, '<%sdevice_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
+    print >>outfile, '<%s%s-event> <%shasAction> <%sdevice_%s>. |%s' %(ex,record[3],ex,ex,id,tsToStr(timestamp))
+    print >>outfile, '<%sdevice_%s> <%s> <%s%sAction>.' %(ex,id,a,ex,'Disk'+record[6]+'ion')
+    #print >>outfile, '<%sdevice_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
     if timestamp.time() < dailyStart or timestamp.time() > dailyEnd:
-        print >>outfile, '<%sdevice_%s> <%s> <%sAfterHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%sdevice_%s> <%s> <%sAfterHourAction>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%sdevice_%s> <%s> <%sInHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%sdevice_%s> <%shasActor> <%s%s>.|%s' %(ex,id,ex,ex,record[3],tsToStr(timestamp))
-    print >>outfile, '<%sdevice_%s> <%sisPerformedOnPC> <%s%s>.|%s' %(ex,id,ex,ex,record[4],tsToStr(timestamp))
+        print >>outfile, '<%sdevice_%s> <%s> <%sInHourAction>.' %(ex,id,a,ex)
+    print >>outfile, '<%sdevice_%s> <%shasActor> <%s%s>.' %(ex,id,ex,ex,record[3])
+    print >>outfile, '<%sdevice_%s> <%sisPerformedOnPC> <%s%s>.' %(ex,id,ex,ex,record[4])
     if (record[6]=='Connect'):
-        print >>outfile, '<%sdevice_%s> <%sisPerformedWithRemovableDisk> <%sdevice_%s_disk>.|%s' %(ex,id,ex,ex,id,tsToStr(timestamp))
-        print >>outfile, '<%sdevice_%s_disk> <%shasFileTree> "%s"^^<%sstring>.|%s' %(ex,id,ex,record[5].replace('\\','_'),xsd,tsToStr(timestamp))
+        print >>outfile, '<%sdevice_%s> <%sisPerformedWithRemovableDisk> <%sdevice_%s_disk>.' %(ex,id,ex,ex,id)
+        print >>outfile, '<%sdevice_%s_disk> <%shasFileTree> "%s"^^<%sstring>.' %(ex,id,ex,record[5].replace('\\','_'),xsd)
 
 # email, id, date, user, pc, to, cc, bcc, from, activity, size, attachment, content
 #   0,    1,   2,    3,   4,  5,  6,  7,    8,       9,    10,      11
@@ -78,55 +78,55 @@ def email(record,outfile):
     content = record[-1]
     id = record[1][1:len(record[1])-1]
     timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-    print >>outfile, '<%sevent%s> <%shasAction> <%semail_%s>.' %(ex,record[3],ex,ex,id)
-    print >>outfile, '<%semail_%s> <%s> <%sEmail%sAction>.|%s' %(ex,id,a,ex,record[9],tsToStr(timestamp))
-    print >>outfile, '<%semail_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
+    print >>outfile, '<%s%s-event> <%shasAction> <%semail_%s>. |%s' %(ex,record[3],ex,ex,id,tsToStr(timestamp))
+    print >>outfile, '<%semail_%s> <%s> <%sEmail%sAction>.' %(ex,id,a,ex,record[9])
+    #print >>outfile, '<%semail_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
     if timestamp.time() < dailyStart or timestamp.time() > dailyEnd:
-        print >>outfile, '<%semail_%s> <%s> <%sAfterHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%semail_%s> <%s> <%sAfterHourAction>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%semail_%s> <%s> <%sInHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%semail_%s> <%shasActor> <%s%s>.|%s' %(ex,id,ex,ex,record[3],tsToStr(timestamp))
-    print >>outfile, '<%semail_%s> <%sisPerformedOnPC> <%s%s>.|%s' %(ex,id,ex,ex,record[4],tsToStr(timestamp))
+        print >>outfile, '<%semail_%s> <%s> <%sInHourAction>.' %(ex,id,a,ex)
+    print >>outfile, '<%semail_%s> <%shasActor> <%s%s>.' %(ex,id,ex,ex,record[3])
+    print >>outfile, '<%semail_%s> <%sisPerformedOnPC> <%s%s>.' %(ex,id,ex,ex,record[4])
     if record[5]:
         toList = record[5].split(';')
         for item in toList:
-            print >>outfile, '<%semail_%s> <%sto> <%s%s>.|%s'%(ex,id,ex,ex,item,tsToStr(timestamp))
+            print >>outfile, '<%semail_%s> <%sto> <%s%s>.'%(ex,id,ex,ex,item)
             if item[item.find('@'):] == '@dtaa.com':
-                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.' %(ex,item,a,ex)
             else:
-                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.' %(ex,item,a,ex)
     if record[6]:
         ccList = record[6].split(';')
         for item in ccList:
-            print >>outfile, '<%semail_%s> <%scc> <%s%s>.|%s'%(ex,id,ex,ex,item,tsToStr(timestamp))
+            print >>outfile, '<%semail_%s> <%scc> <%s%s>.'%(ex,id,ex,ex,item)
             if item[item.find('@'):] == '@dtaa.com':
-                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.' %(ex,item,a,ex)
             else:
-                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.' %(ex,item,a,ex)
     if record[7]:
         bccList = record[7].split(';')
         for item in bccList:
-            print >>outfile, '<%semail_%s> <%sbcc> <%s%s>.|%s'%(ex,id,ex,ex,item,tsToStr(timestamp))
+            print >>outfile, '<%semail_%s> <%sbcc> <%s%s>.'%(ex,id,ex,ex,item)
             if item[item.find('@'):] == '@dtaa.com':
-                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.' %(ex,item,a,ex)
             else:
-                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.|%s' %(ex,item,a,ex,tsToStr(timestamp))
+                print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.' %(ex,item,a,ex)
 
-    print >>outfile, '<%semail_%s> <%sfrom> <%s%s>.|%s'%(ex,id,ex,ex,record[8],tsToStr(timestamp))
+    print >>outfile, '<%semail_%s> <%sfrom> <%s%s>.'%(ex,id,ex,ex,record[8])
     if record[8][record[8].find('@'):] == '@dtaa.com':
-        print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.|%s' %(ex,record[8],a,ex,tsToStr(timestamp))
+        print >>outfile, '<%semail_%s> <%s> <%sInternalEmailAddress>.' %(ex,record[8],a,ex)
     else:
-        print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.|%s' %(ex,record[8],a,ex,tsToStr(timestamp))
+        print >>outfile, '<%semail_%s> <%s> <%sNotInternalEmailAddress>.' %(ex,record[8],a,ex)
 
-    print >>outfile, '<%semail_%s> <%shasEmailSize> "%s bytes"^^<%sstring>.|%s'%(ex,id,ex,record[10],xsd,tsToStr(timestamp))
+    print >>outfile, '<%semail_%s> <%shasEmailSize> "%s bytes"^^<%sstring>.'%(ex,id,ex,record[10],xsd)
     if record[11]:
         attachmentList = record[11].split(';')
         for i in range(len(attachmentList)):
             filename = attachmentList[i][:attachmentList[i].find('(')].replace('\\','_')
             attachmentSize = attachmentList[i][attachmentList[i].find('(')+1:item.find(')')]
-            print >>outfile, '<%semail_%s> <%shasEmailAttachment> <%s%s>.|%s'%(ex,id,ex,ex,filename,tsToStr(timestamp))
-            print >>outfile, '<%sattachment%s> <%shasSize> "%s bytes"^^<%sstring>.|%s'%(ex,str(i+1),ex,attachmentSize,xsd,tsToStr(timestamp))
-    print >>outfile, '<%semail_%s> <%shasContent> "%s"^^<%sstring>.|%s'%(ex,id,ex,content,xsd,tsToStr(timestamp))
+            print >>outfile, '<%semail_%s> <%shasEmailAttachment> <%s%s>.'%(ex,id,ex,ex,filename)
+            print >>outfile, '<%sattachment%s> <%shasSize> "%s bytes"^^<%sstring>.'%(ex,str(i+1),ex,attachmentSize,xsd)
+    print >>outfile, '<%semail_%s> <%shasContent> "%s"^^<%sstring>.'%(ex,id,ex,content,xsd)
 
 # file, id, date, user, pc, filename, activity, to_removable_media, from_removable_media, content
 #    0,  1,   2,   3,   4,    5,         6,               7,                8,               9
@@ -134,26 +134,26 @@ def file(record,outfile):
     content = record[-1]
     id = record[1][1:len(record[1])-1]
     timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-    print >>outfile, '<%sevent%s> <%shasAction> <%sfile_%s>.' %(ex,record[3],ex,ex,id)
-    print >>outfile, '<%sfile_%s> <%s> <%sFile%sAction>.|%s' %(ex,id,a,ex,record[6][5:],tsToStr(timestamp))
-    print >>outfile, '<%sfile_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
+    print >>outfile, '<%s%s-event> <%shasAction> <%sfile_%s>. |%s' %(ex,record[3],ex,ex,id,tsToStr(timestamp))
+    print >>outfile, '<%sfile_%s> <%s> <%sFile%sAction>.' %(ex,id,a,ex,record[6][5:])
+    #print >>outfile, '<%sfile_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
     if timestamp.time() < dailyStart or timestamp.time() > dailyEnd:
-        print >>outfile, '<%sfile_%s> <%s> <%sAfterHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s> <%s> <%sAfterHourAction>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%sfile_%s> <%s> <%sInHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%sfile_%s> <%shasActor> <%s%s>.|%s' %(ex,id,ex,ex,record[3],tsToStr(timestamp))
-    print >>outfile, '<%sfile_%s> <%sisPerformedOnPC> <%s%s>.|%s' %(ex,id,ex,ex,record[4],tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s> <%s> <%sInHourAction>.' %(ex,id,a,ex)
+    print >>outfile, '<%sfile_%s> <%shasActor> <%s%s>.' %(ex,id,ex,ex,record[3])
+    print >>outfile, '<%sfile_%s> <%sisPerformedOnPC> <%s%s>.' %(ex,id,ex,ex,record[4])
     filename = record[5].replace('\\','_')
-    print >>outfile, '<%sfile_%s> <%shasFile> <%s%s>.|%s'%(ex,id,ex,ex,filename,tsToStr(timestamp))
+    print >>outfile, '<%sfile_%s> <%shasFile> <%s%s>.'%(ex,id,ex,ex,filename)
     if record[7]=='True':
-        print >>outfile, '<%sfile_%s_file> <%s> <%sFileToRemovableMedia>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s_file> <%s> <%sFileToRemovableMedia>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%sfile_%s_file> <%s> <%sNotFileToRemovableMedia>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s_file> <%s> <%sNotFileToRemovableMedia>.' %(ex,id,a,ex)
     if record[8]=='True':
-        print >>outfile, '<%sfile_%s_file> <%s> <%sFileFromRemovableMedia>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s_file> <%s> <%sFileFromRemovableMedia>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%sfile_%s_file> <%s> <%sNotFileFromRemovableMedia>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%sfile_%s_file> <%shasContent> "%s"^^<%sstring>.|%s' %(ex,id,ex,content,xsd,tsToStr(timestamp))
+        print >>outfile, '<%sfile_%s_file> <%s> <%sNotFileFromRemovableMedia>.' %(ex,id,a,ex)
+    print >>outfile, '<%sfile_%s_file> <%shasContent> "%s"^^<%sstring>.' %(ex,id,ex,content,xsd)
 
 # http, id, date, user, pc, url, activity, content
 #  0,    1,   2,   3,   4,   5,     6,        7
@@ -161,26 +161,26 @@ def http(record,outfile):
     content = record[-1]
     id = record[1][1:len(record[1])-1]
     timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-    print >>outfile, '<%sevent%s> <%shasAction> <%shttp_%s>.' %(ex,record[3],ex,ex,id)
-    print >>outfile, '<%shttp_%s> <%s> <%s%sAction>.|%s' %(ex,id,a,ex,record[6].replace(' ',''),tsToStr(timestamp))
-    print >>outfile, '<%shttp_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
+    print >>outfile, '<%s%s-event> <%shasAction> <%shttp_%s>. |%s' %(ex,record[3],ex,ex,id,tsToStr(timestamp))
+    print >>outfile, '<%shttp_%s> <%s> <%s%sAction>.' %(ex,id,a,ex,record[6].replace(' ',''))
+    #print >>outfile, '<%shttp_%s> <%shasTimestamp> "%s-05:00"^^<%sdateTime>.|%s' %(ex,id,ex,tsToStr(timestamp),xsd,tsToStr(timestamp))
     if timestamp.time() < dailyStart or timestamp.time() > dailyEnd:
-        print >>outfile, '<%shttp_%s> <%s> <%sAfterHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
+        print >>outfile, '<%shttp_%s> <%s> <%sAfterHourAction>.' %(ex,id,a,ex)
     else:
-        print >>outfile, '<%shttp_%s> <%s> <%sInHourAction>.|%s' %(ex,id,a,ex,tsToStr(timestamp))
-    print >>outfile, '<%shttp_%s> <%shasActor> <%s%s>.|%s' %(ex,id,ex,ex,record[3],tsToStr(timestamp))
-    print >>outfile, '<%shttp_%s> <%sisPerformedOnPC> <%s%s>.|%s' %(ex,id,ex,ex,record[4],tsToStr(timestamp))
-    print >>outfile, '<%shttp_%s> <%shasURL> <%s>.|%s' %(ex,id,ex,record[5],tsToStr(timestamp))
+        print >>outfile, '<%shttp_%s> <%s> <%sInHourAction>.' %(ex,id,a,ex)
+    print >>outfile, '<%shttp_%s> <%shasActor> <%s%s>.' %(ex,id,ex,ex,record[3])
+    print >>outfile, '<%shttp_%s> <%sisPerformedOnPC> <%s%s>.' %(ex,id,ex,ex,record[4])
+    print >>outfile, '<%shttp_%s> <%shasURL> <%s>.' %(ex,id,ex,record[5])
     domainName = record[5][:record[5].find('/',7)+1]
     if domainName in cloudStorageWebsites:
-        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%scloudstoragewebsite>.|%s' %(record[5],ex,ex,tsToStr(timestamp))
+        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%scloudstoragewebsite>.' %(record[5],ex,ex)
     elif domainName in hacktivistWebsites:
-        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%shacktivistwebsite>.|%s' %(record[5],ex,ex,tsToStr(timestamp))
+        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%shacktivistwebsite>.' %(record[5],ex,ex)
     elif domainName in jobHuntingWebsites:
-        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%sjobhuntingwebsite>.|%s' %(record[5],ex,ex,tsToStr(timestamp))
+        print >>outfile, '<%s> <%swhoseDomainNameIsA> <%sjobhuntingwebsite>.' %(record[5],ex,ex)
     else:
-        print >>outfile, '<%s> <%s> <%sneuturalwebsite>.|%s' %(record[5],a,ex,tsToStr(timestamp))
-    print >>outfile, '<%shttp_%s> <%shasContent> "%s"^^<%sstring>.|%s' %(ex,id,ex,content,xsd,tsToStr(timestamp))
+        print >>outfile, '<%s> <%s> <%sneuturalwebsite>.' %(record[5],a,ex)
+    print >>outfile, '<%shttp_%s> <%shasContent> "%s"^^<%sstring>.' %(ex,id,ex,content,xsd)
 
 
 def extract(user):
@@ -243,7 +243,7 @@ def annotate(user):
 	f = open('intermediate/'+user+'_aggregated.csv')
 	outfile = open(user+'-annotation.txt','w')
 	userID = f.readline().split(',')[3]
-	print >>outfile, '<%s%s> <%sisInvolvedIn> <%sevent%s>.' %(ex,userID,ex,ex,userID)
+	print >>outfile, '<%s%s> <%sisInvolvedIn> <%s%s-event>.' %(ex,userID,ex,ex,userID)
 	f.seek(0,0)
 	deviceUsageCounter = 0
 	isDeviceConnected = False
@@ -273,10 +273,10 @@ def annotate(user):
 	        record = record[:record.find('"')].split(',')
 	        record.append(content)
 	        file(record,outfile)
-	        if connectedDevice:
-	            id = record[1][1:len(record[1])-1]
-	            timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-	            print >>outfile, '<%s%s> <%sstartsNoEarlierThanEndingOf> <%s%s>.|%s' %(ex,id,ex,ex,connectedDevice,tsToStr(timestamp))
+	        #if connectedDevice:
+	        #    id = record[1][1:len(record[1])-1]
+	        #    timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
+            #    print >>outfile, '<%s%s> <%sstartsNoEarlierThanEndingOf> <%s%s>.|%s' %(ex,id,ex,ex,connectedDevice,tsToStr(timestamp))
 	    elif type == 'http':
 	        content = record[record.find('"'):len(record)-1].replace('"','')
 	        record = record[:record.find('"')].split(',')
@@ -288,7 +288,8 @@ def annotate(user):
 
 	if deviceUsageCounter > usbDriveUsageFrequency:
 	    timestamp = datetime.datetime.strptime(record[2],'%m/%d/%Y %H:%M:%S')
-	    print >>outfile, '<%s%s> <%s> <%sExcessiveRemovableDriveUser>.|%s' %(ex,userID,a,ex,tsToStr(timestamp))
+	    print >>outfile, '<%s%s> <%s> <%sExcessiveRemovableDriveUser>.' %(ex,userID,a,ex)
+        #print >>outfile, '<%s%s> <%s> <%sExcessiveRemovableDriveUser>.|%s' %(ex,userID,a,ex,tsToStr(timestamp))
 	f.close()
 	outfile.close()
 
