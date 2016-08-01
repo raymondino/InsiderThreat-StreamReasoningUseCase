21cfec8366e5bcd9a620a6c041d954ff9fafd255
Rui Yan <raymondino@users.noreply.github.com>
2016-08-01 16:00:07 -0400

    update jobhunting webiste pool

--- mychoco/globals.py
+++ mychoco/globals.py
@@ -5,7 +5,7 @@ MAXTIME = datetime.datetime(9999, 12, 31, 23, 59, 59)
 
 cloudStorageWebsites = ['http://www.4shared.com/','http://1and1.com/','https://archive.org/','http://bluehost.com/','http://bp.blogspot.com/','http://yousendit.com/','http://yfrog.com/','http://webs.com/','http://twitpic.com/','http://soundcloud.com/','http://secureserver.net/','http://custhelp.com/','http://megaupload.com/','http://megaclick.com/','http://hostgator.com/','http://flippa.com/','https://www.dropbox.com/']
 hacktivistWebsites = ['http://wikileaks.org/']
-jobHuntingWebsites = ['http://careerbuilder.com/','http://craigslist.org/','http://simplyhired.com/','http://monster.com/','http://jobhuntersbible.com/','http://job-hunt.org/','http://indeed.com/','http://linkedin.com/']
+jobHuntingWebsites = ['http://lockheedmartin.com/','http://careerbuilder.com/','http://craigslist.org/','http://simplyhired.com/','http://monster.com/','http://jobhuntersbible.com/','http://job-hunt.org/','http://indeed.com/','http://linkedin.com/']
 ex = 'http://tw.rpi.edu/ontology/DataExfiltration/'
 a = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
 xsd = 'http://www.w3.org/2001/XMLSchema#'
